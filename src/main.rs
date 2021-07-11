1863b4ffc4e501b8a45f78286cae952b7f9f7b75
Danny Gim <dohyoung.gim@linecorp.com>
2021-07-11 21:31:40 +0900

    add study_1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::str::FromStr;
+
+use study::study_1;
+mod study;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        let num: i32 = i32::from_str(&args[1]).unwrap_or(0);
+
+        match num {
+            1 => study_1::run(),
+            2 => print!("2 is selected"),
+            _ => print_help(),
+        }
+    } else {
+        print_help();
+    }
+}
+
+fn print_help() {
+    let mut f = match File::open("help.txt") {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file {:?}", error),
+    };
+
+    let mut buffer = String::new();
+    let _ = match f.read_to_string(&mut buffer) {
+        Ok(size) => size,
+        Err(error) => panic!("Problem reading the file {:?}", error),
+    };
+    print!("{}", buffer);
 }
